3368c28e0162a1b067847d896ae97a181ccad3d6
Joe Shelby <joe.shelby@oracle.com>
2021-08-15 18:53:49 -0400

    AuthExchangeActions ported to ts

---
+++ packages/core/src/AuthExchangeActions.ts
@@ -0,0 +1,56 @@
+import Tea from '../js/utils/Tea';
+
+const AuthExchangeActions = {
+  sourceSubmit: function (code: string) {
+    try {
+      const transmissionDigits = code.substring(0, 5);
+      // AH - bypasses the store by just going to local storage
+      const payloadData = {
+        configs: window.localStorage['subsonic.credentials']
+      };
+      const payload = Tea.encrypt(JSON.stringify(payloadData), code);
+      const myHeaders = new Headers();
+
+      const fd = new FormData();
+      fd.set('code', transmissionDigits);
+      fd.set('payload', payload);
+
+      const myInit = {
+        method: 'POST',
+        headers: myHeaders,
+        mode: 'cors' as RequestMode,
+        cache: 'default' as RequestCache,
+        body: fd
+      };
+      return fetch('https://subfiresuite.com/sflink/sflink.php', myInit);
+    } catch (e) {
+      console.error('oops', e);
+      return new Promise((res, rej) => {
+        rej(e);
+      });
+    }
+  },
+
+  clientRequest: function (code) {
+    const transmissionDigits = code.substring(0, 5);
+
+    const myHeaders = new Headers();
+    const myInit = {
+      method: 'GET',
+      headers: myHeaders,
+      mode: 'cors' as RequestMode,
+      cache: 'default' as RequestCache
+    };
+
+    return fetch('https://subfiresuite.com/sflink/sflink.php?code=' + transmissionDigits, myInit);
+  },
+
+  allocateDigits: function () {
+    const length = 7;
+    const randNumber = Math.floor(Math.random() * (9 * Math.pow(10, length - 1) - 1) + Math.pow(10, length - 1));
+    const digits = '' + randNumber;
+    return digits;
+  }
+};
+
+export default AuthExchangeActions;
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,6 @@ export * as utils from '../js/utils/utils';
 export * as SubsonicTypes from './SubsonicTypes';
 export * as SubsonicCache from './SubsonicCache';
 export * as Subsonic from './Subsonic';
+export * as AuthExchangeActions from './AuthExchangeActions';
 
 export * as default from './Subsonic';
