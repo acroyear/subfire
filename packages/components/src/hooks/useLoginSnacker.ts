c79c6ad50279e9765b6f32368b5015168af4715a
Joe Shelby <joe.shelby@oracle.com>
2021-08-29 10:09:00 -0400

    login snacker restored (with improvement - don't show partial until it really is clear the full isn't happening fast)

---
+++ packages/components/src/hooks/useLoginSnacker.ts
@@ -0,0 +1,41 @@
+import { useRef, useEffect, RefObject } from 'react';
+
+import { LoginStates } from '../SubfireTypes';
+import { useSubsonic } from './SubsonicContext';
+import { SnackbarKey, useSnackbar } from 'notistack';
+import { UseTimeoutFnReturn } from 'react-use/lib/useTimeoutFn';
+
+function useLoginSnacker() {
+  const { loginState } = useSubsonic();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const snackRef = useRef<SnackbarKey>();
+
+  const timeoutRef = useRef<number>();
+
+  useEffect(() => {
+    if (snackRef.current) {
+      closeSnackbar(snackRef.current);
+      snackRef.current = null;
+    }
+    if (loginState === LoginStates.notLoggedIn) {
+      snackRef.current = enqueueSnackbar('Logged Out', {
+        variant: 'error'
+      });
+    }
+    if (loginState === LoginStates.partiallyLoggedIn) {
+      if (timeoutRef.current) window.clearTimeout(timeoutRef.current);
+      timeoutRef.current = window.setTimeout(() => {
+        snackRef.current = enqueueSnackbar('Login Partialy Successful', {
+          variant: 'warning'
+        });
+      }, 5000);
+    } else if (loginState === LoginStates.fullyLoggedIn) {
+      if (timeoutRef.current) window.clearTimeout(timeoutRef.current);
+      snackRef.current = enqueueSnackbar('Login Successful', {
+        variant: 'success'
+      });
+    }
+  }, [enqueueSnackbar, closeSnackbar, loginState]);
+}
+
+export default useLoginSnacker;
--- packages/components/src/hooks/useSubsonic.stories.tsx
+++ packages/components/src/hooks/useSubsonic.stories.tsx
@@ -1,42 +1,67 @@
-import React, { useEffect } from 'react';
-import { useSubsonic, SubsonicProvider, buildProcessEnvCredentials } from './SubsonicContext';
+import React, { useEffect } from "react";
+import {
+  useSubsonic,
+  SubsonicProvider,
+  buildProcessEnvCredentials,
+} from "./SubsonicContext";
+import useLoginSnacker from "./useLoginSnacker";
+import { SnackbarProvider, SnackbarKey } from "notistack";
+import { Button } from "@material-ui/core";
 
 export default {
-  title: 'hooks/SubsonicContextAlone'
+  title: "hooks/SubsonicContextAlone",
 };
 
-const SubsonicWrapper: React.FC<any> = props => {
+const SubsonicWrapper: React.FC<any> = (props) => {
   console.warn(props);
   return (
-    <SubsonicProvider clientName="SubfireStorybook" embeddedCredentials={props.embeddedCredentials}>
+    <SubsonicProvider
+      clientName="SubfireStorybook"
+      embeddedCredentials={props.embeddedCredentials}
+    >
       {props.children}
     </SubsonicProvider>
   );
 };
 
 const Inner = (_props: any) => {
-  const { loginState, isLoggedIn, musicFolderLoaded, musicFolderId, tryAgain, SubsonicCache: cache, Subsonic } = useSubsonic();
+  const {
+    loginState,
+    isLoggedIn,
+    musicFolderLoaded,
+    musicFolderId,
+    tryAgain,
+    SubsonicCache: cache,
+    Subsonic,
+  } = useSubsonic();
 
   useEffect(() => {
     if (isLoggedIn) {
-      console.log('subsonic', Subsonic);
-      console.log('musicFolderLoaded', musicFolderLoaded, musicFolderId);
-      console.log('MusicFolders', cache.MusicFolders);
-      console.log('MusicDirectoryIndexes', cache.MusicDirectoryIndexes);
-      console.log('ArtistIndexes', cache.ArtistIndexes);
-      console.log('ArtistsById', cache.ArtistsById);
-      console.log('ArtistsByName', cache.ArtistsByName);
-      console.log('ArtistNames', cache.ArtistNames);
-      console.log('Genres', cache.Genres);
+      console.log("subsonic", Subsonic);
+      console.log("musicFolderLoaded", musicFolderLoaded, musicFolderId);
+      console.log("MusicFolders", cache.MusicFolders);
+      console.log("MusicDirectoryIndexes", cache.MusicDirectoryIndexes);
+      console.log("ArtistIndexes", cache.ArtistIndexes);
+      console.log("ArtistsById", cache.ArtistsById);
+      console.log("ArtistsByName", cache.ArtistsByName);
+      console.log("ArtistNames", cache.ArtistNames);
+      console.log("Genres", cache.Genres);
     } else {
       console.warn(loginState);
     }
-  }, [cache, Subsonic, isLoggedIn, musicFolderId, musicFolderLoaded, loginState]);
+  }, [
+    cache,
+    Subsonic,
+    isLoggedIn,
+    musicFolderId,
+    musicFolderLoaded,
+    loginState,
+  ]);
 
   return (
     <>
       <span onClick={tryAgain}>try again?</span>
-      <hr/>
+      <hr />
       <p>{JSON.stringify(cache)}</p>
     </>
   );
@@ -46,12 +71,50 @@ export const ContextDemo = (_props: any) => {
   const credentials = buildProcessEnvCredentials();
   console.warn(credentials);
   return (
+    <SubsonicWrapper embeddedCredentials={credentials}>
+      <div>
+        <div>
+          <Inner />
+        </div>
+      </div>
+    </SubsonicWrapper>
+  );
+};
+
+const Snacker: React.FC = (props) => {
+  useLoginSnacker();
+  return <>{props.children}</>;
+};
+
+export const SnackerDemo = (_props: any) => {
+  const credentials = buildProcessEnvCredentials();
+  console.warn(credentials);
+
+  const notistackRef = React.createRef<SnackbarProvider>();
+  const onClickDismiss = (key: SnackbarKey) => {
+    notistackRef.current.closeSnackbar(key);
+  };
+
+  return (
+    <SnackbarProvider
+      ref={notistackRef}
+      action={(key) => (
+        <Button
+          style={{ color: "#ffffff" }}
+          size="small"
+          onClick={() => onClickDismiss(key)}
+        >
+          Dismiss
+        </Button>
+      )}
+    >
       <SubsonicWrapper embeddedCredentials={credentials}>
         <div>
-          <div>
-            <Inner />
-          </div>
+          <Snacker>
+            <p>Hi mom...</p>
+          </Snacker>
         </div>
       </SubsonicWrapper>
+    </SnackbarProvider>
   );
 };
