e2d9d3e27a33b3261893817e543f10d6ae103a91
Joe Shelby <jws.fbmm@gmail.com>
2021-10-16 20:42:04 -0400

    loader being built

---
+++ packages/core/src/SubsonicLoader.ts
@@ -0,0 +1,75 @@
+import { SongList } from './SubsonicTypes';
+import { Subsonic, SubsonicSongLoader } from './Subsonic';
+
+interface Params {
+    type: string
+    id?: string
+    mode?: string
+    bookmarkId?: string
+}
+
+interface ParamBuilder {
+    (params?: Params): any
+}
+
+interface LoaderBuilder {
+    (params?: Params): SubsonicSongLoader
+}
+
+interface LoaderDescriptor {
+    method: LoaderBuilder
+    param1?: ParamBuilder
+    param2?: ParamBuilder
+    param3?: ParamBuilder
+    param4?: ParamBuilder
+}
+
+interface LoaderDescriptors {
+    [key: string]: LoaderDescriptor
+}
+
+const loaders:LoaderDescriptors = {
+    album: {
+        method: () => Subsonic.getAlbumSongs,
+        param1: (params: Params) => params.id
+    },
+    artist: {
+        method: () => Subsonic.getArtistSongs,
+        param1: (params: Params) => params.id
+    },
+    artistAction: {
+        method: (params: Params) => {
+            if (params.mode === 'shuffleAlbums') {
+                return Subsonic.getArtistAlbumSongs
+            } else if (params.mode === 'radio') {
+                return Subsonic.getTopSimilarSongs
+            } else if (params.mode === 'top') {
+                return Subsonic.getTopSongs
+            }
+            return Subsonic.getArtistSongs
+        },
+        param1: (params: Params) => params.id,
+        param2: (params: Params) => {
+            if (params.mode === 'radio') return 200;
+            return undefined;
+        },
+        param3: (params: Params) => {
+            if (params.mode === 'radio') return 0;
+            return undefined;
+        }
+    }
+}
+
+export const SubsonicLoader = async (params: Params, shuffle: boolean = false) : Promise<SongList> => {
+    const ld = loaders[params.type];
+    if (!ld) return [];
+
+    const method = ld.method(params);
+    const p1 = (ld.param1 && ld.param1(params)) || undefined;
+    const p2 = (ld.param1 && ld.param2(params)) || undefined;
+    const p3 = (ld.param1 && ld.param3(params)) || undefined;
+    const p4 = (ld.param1 && ld.param4(params)) || undefined;
+
+    const songList = await method(p1, p2, p3, p4);
+    const rv = Subsonic.applyShuffleAndFlatten(songList, shuffle);
+}
