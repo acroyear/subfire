dd23fbdbf3555038edadb02acae335e68e94c6b7
Joe Shelby <jws.fbmm@gmail.com>
2021-11-20 13:41:17 -0500

    protect player routes

--- packages/m4/src/Routes.tsx
+++ packages/m4/src/Routes.tsx
@@ -1,10 +1,15 @@
 import { LoaderSwitch, ModalSwitch } from '@subfire/components';
+import { useSubsonic } from '@subfire/hooks';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Pages from './pages';
 // console.log(Pages.map(p => p.path));
 
 export const Routes = (_props: any) => {
+    const { isLoggedIn } = useSubsonic();
+    if (!isLoggedIn) {
+        return <>Loading...</>;
+    }
     return (
         <>
             <Switch>
